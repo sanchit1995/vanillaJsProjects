d58e033ac3cbd81e56b6103048d70718d96b5585
sanchit1995 <35392291+sanchit1995@users.noreply.github.com>
2020-04-26 23:41:21 +0530

    Meal Finder
    Meal Finder using fetch api TheMealDB

--- app.js
+++ app.js
@@ -1,73 +1,62 @@
-const main=document.getElementById('main'),
-    addUserBtn=document.getElementById('add-user'),
-    doubleBtn=document.getElementById('double'),
-    showMillionariesBtn=document.getElementById('show-millionaries'),
-    sortBtn=document.getElementById('sort');
-    calculateWealthBtn=document.getElementById('calculate-wealth');
-
-let data=[]
-//To fetch the random users 
-function getRandomUser(){
-    fetch('https://randomuser.me/api').then(res=>res.json()).then(data=>{
-    const user=data.results[0];
-    const newUser={
-        Name:`${user.name.first} ${user.name.last}`,
-        Money:Math.floor(Math.random()*1000000)
-    };
-    console.log(newUser);
-    addData(newUser);
-    });
-}
-//Double the Money
-function double(){
-    data=data.map(user=>{
-        return {...user,Money:user.Money*2};
+const search=document.getElementById('search'),
+    resultHeading=document.getElementById('result-heading'),
+    submit=document.getElementById('submit'),
+    mealsEl=document.getElementById('meals'),
+    random=document.getElementById('random'),
+    disIngredient=document.getElementById('dis-ing');
+//for searching a meal by name
+function searchMeal(e){
+    e.preventDefault();
+    const meal=search.value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        if(data.meals===null){
+            resultHeading.innerHTML=`No such item found ${meal}`;
+        }
+        else{    
+            resultHeading.innerHTML=data.meals.map(meal=>`
+                <h2 style="color:orange;">Search result for ${meal.strMeal}</h2>
+                <h2 style="font-size:28px;">Cooking instructions:<br>
+                <div style="margin-bottom:2rem;"> ${meal.strInstructions}</div>
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+            `)
+        }
+        const meal=data.meals[0]
+        getIngredient(meal);
     })
-    updateDom();
+}   
+//getting a random meal
+function getRandonMeal(){
+    mealsEl.innerHTML='';
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res=>res.json())
+    .then(data=>{
+        let i=2;
+        resultHeading.innerHTML=data.meals.map(meal=>`
+            <h2 style="color:orange;">Search result for ${meal.strMeal}</h2>
+            <div style="font-size:28px; color:orange; margin-bottom:1rem;">Cooking instructions:</div>
+            <div style="margin-bottom:2rem;"> ${meal.strInstructions}</div>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>  
+        `)
+        const meal=data.meals[0]
+        getIngredient(meal);
+    })
 }
-//For the sort accoring to Money
-function sortByRichest(){
-    data=data.sort((a,b)=>{
-        return b.Money-a.Money;
-    });
-    updateDom();
-}
-//To show the millionaries
-function showMillionarie(){
-    data=data.filter(user=>{
-        return user.Money>1000000;
-    });
-    updateDom();
-}
-//To calculate entire Wealth
-function entireWealth(){
-    const wealth=data.reduce((acc,user)=> (acc+=user.Money),0);
-    const wealthEl=document.createElement('div');
-    wealthEl.innerHTML=`<h3>The total Wealth is <strong>${formatMoney(wealth)}</strong></h3>`;
-    main.appendChild(wealthEl);
-}
-//To add the person to array
-function addData(obj){
-    data.push(obj);
-    updateDom();
-}    
-//To render the items on DOM
-function updateDom(providedData=data){
-    main.innerHTML=`<h2><strong>Person</strong>wealth</h2>`;
-    providedData.forEach(items => {
-       const element=document.createElement('div');
-       element.classList.add('parent');
-       element.innerHTML=`<strong>${items.Name}</strong>${formatMoney(items.Money)}`;
-       main.appendChild(element); 
-    });
-//To format the wealth     
-}    
-function formatMoney(number) {
-    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-  }
-//Event Listener  
-addUserBtn.addEventListener('click',getRandomUser);
-doubleBtn.addEventListener('click',double);
-sortBtn.addEventListener('click',sortByRichest);
-showMillionariesBtn.addEventListener('click',showMillionarie);
-calculateWealthBtn.addEventListener('click',entireWealth);
+//getting the ingredient of the meal
+function getIngredient(meal){
+        const ingredients=[];
+        for(let i=1;i<=20;i++){
+            if(`${meal[`strIngredient${i}`]}`===""){
+                break;
+            }
+            else{
+            ingredients.push(`${meal[`strIngredient${i}`]}`);}
+        }
+        disIngredient.innerHTML='Ingredients reqiured are: ';
+        mealsEl.innerHTML=ingredients;
+    }
+//Event Listeners
+submit.addEventListener('submit',searchMeal);
+random.addEventListener('click',getRandonMeal);
